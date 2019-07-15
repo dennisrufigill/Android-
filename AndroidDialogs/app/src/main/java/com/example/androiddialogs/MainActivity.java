c39dd84b5bb718d59f4c5d04bf3036d9e352e9bb
dennisrufigill <rufidennis@gmail.com>
2019-07-15 12:26:18 +0500

    Android Alert and Progress Dialogs

---
+++ AndroidDialogs/app/src/main/java/com/example/androiddialogs/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.androiddialogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/*
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert Dialog");
+        alertDialog.setMessage("Welcome to Android");
+        alertDialog.setIcon(R.drawable.checked);
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialog.show();
+
+
+*/
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+       // AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("Alert Dialog");
+        builder.setTitle("Android Dialogs");
+        builder.setIcon(R.drawable.checked);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You Clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "You Clicked Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+
+builder.show();
+
+
+
+/*
+
+        AlertDialog.Builder alertDialog2 = new AlertDialog.Builder(
+                MainActivity.this);
+
+// Setting Dialog Title
+        alertDialog2.setTitle("Confirm Delete...");
+
+// Setting Dialog Message
+        alertDialog2.setMessage("Are you sure you want delete this file?");
+
+// Setting Icon to Dialog
+        alertDialog2.setIcon(R.drawable.checked);
+
+// Setting Positive "Yes" Btn
+        alertDialog2.setPositiveButton("YES",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                        Toast.makeText(getApplicationContext(),
+                                "You clicked on YES", Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+// Setting Negative "NO" Btn
+        alertDialog2.setNegativeButton("NO",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Write your code here to execute after dialog
+                        Toast.makeText(getApplicationContext(),
+                                "You clicked on NO", Toast.LENGTH_SHORT)
+                                .show();
+                        dialog.cancel();
+                    }
+                });
+
+// Showing Alert Dialog
+        alertDialog2.show();
+
+        */
+    }
+}
